772a9fd7f6ed5e5b3b8dbf9d62f627b7ed13057a
LucioNessier <nessier.lucio@gmail.com>
2021-07-21 15:02:44 -0300

    Finalizado navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from '../Button';
 import { MenuItems } from './MenuItems';
 import './Navbar.css'
 
@@ -34,6 +35,7 @@ class Navbar extends Component {
                     })}
 
                 </ul>
+                <Button>Sign Up</Button>
             </nav>
         )
     }
