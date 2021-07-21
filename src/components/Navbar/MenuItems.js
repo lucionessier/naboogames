0f4aa2f32852107e1de217173c225e166638bd24
LucioNessier <nessier.lucio@gmail.com>
2021-07-21 13:02:25 -0300

    Avanzando con el NavBar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,31 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+
+    {
+        title: 'Productos',
+        url: '#',
+        cName: 'nav-links',
+    },
+
+    {
+        title: 'Cómo funciona',
+        url: '#',
+        cName: 'nav-links',
+    },
+
+    {
+        title: 'Contactanos',
+        url: '#',
+        cName: 'nav-links',
+    },
+
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile',
+    }
+]
